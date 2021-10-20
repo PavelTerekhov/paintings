0daded2f8b5d0a7a9f48f9035b5c72c45e23916f
Pavel <pavel.a.terekhov@gmail.com>
2021-10-20 19:53:45 +0700

    start of the form module for submitting data

---
+++ src/js/modules/forms.js
@@ -0,0 +1,92 @@
+// import checkNumInputs from './checkNumInputs';
+
+import { setTimeout } from "core-js/core";
+
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+
+    // checkNumInputs('input[name="user_phone"]');
+    
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...',
+        spinner: 'assets/img/spiner.gif',
+        ok: 'assets/img/ok.png',
+        fail: 'assets/img/fail.png'
+    };
+
+    const path = {
+        designer: 'assets/server.php',
+        question: 'assets/question.php'
+    }
+
+    const postData = async (url, data) => {
+
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.parentNode.appendChild(statusMessage);
+            item.classList.add('animated', 'fadeOutUp');
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 400);
+
+            let statusImg = document.createElement('img');
+            statusImg.setAttribute('src', message.spinner);
+            statusImg.classList.add('animated', 'fadeInUp');
+            statusMessage.appendChild(statusImg);
+
+            let textMassege = documenr.createElement('div');
+            textMassege.textContent = message.loading;
+            statusMessage.appendChild(textMassege);
+
+        const formData = new FormData(item);
+        let api;
+        item.closest('.popup-design') ? api = path.designer : api = path.question;  //поиск выше по иерархии (родителей) класса 
+        console.log(api);
+
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusImg.setAttribute('src', massege.ok);
+                    textMessage.textContent = message.success;
+                })
+                .catch(() => {
+                    statusImg.setAttribute('src', massege.fail);
+                    statusMessage.textContent = message.failure;
+                   
+                })
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                        item.style.display = 'block';
+                        item.classList.remove('fadeOutUp');
+                        item.classList.add('fadeInUp');
+                    }, 5000);
+                });
+        });
+    });
+};
+
+export default forms;
